51e8352bf36a5965e8d3945b4cc7803a441871fa
kokov1ch <avk4125@mail.ru>
2023-09-21 04:14:38 +1000

    Add jwt and refresh token

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
