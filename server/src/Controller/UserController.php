421b66245d0b3ef0ed5d65d15af81f9239feeadb
kokov1ch <avk4125@mail.ru>
2023-09-23 03:00:09 +1000

    Add CRUD for User

--- server/src/Controller/UserController.php
+++ server/src/Controller/UserController.php
@@ -93,6 +93,103 @@ class UserController extends ApiController
         }
     }
 
+    #[Route(name: 'delete', methods: ['DELETE'])]
+    public function deleteUsers(Request $request): JsonResponse
+    {
+        // TODO: Сделать каскадное удаление тасков
+
+        $request = $request->toArray();
+
+        try {
+            $userIds = $request['user_ids'];
+
+            $this->em->beginTransaction();
+            foreach ($userIds as $userId) {
+                $user = $this->userRepository->find($userId);
+                if (!$user) {
+                    $this->em->rollback();
+                    return $this->respondNotFound("User not found");
+                }
+                $this->em->remove($user);
+            }
+            $this->em->flush();
+            $this->em->commit();
+
+            return $this->respondWithSuccess("Users deleted successfully");
+        } catch (Exception) {
+            return $this->respondValidationError();
+        }
+    }
+
+    #[Route('/{userId}', name: 'get_by_id', requirements: ['userId' => '\d+'], methods: ['GET'])]
+    public function getUserById(UserPreviewer $userPreviewer, int $userId): JsonResponse
+    {
+        $user = $this->userRepository->find($userId);
+        if (!$user) {
+            return $this->respondNotFound("User not found");
+        }
+
+        $this->setSoftDeleteable($this->em, false);
+
+        return $this->response($userPreviewer->preview($user));
+    }
+
+    #[Route('/{userId}', name: 'put_by_id', requirements: ['userId' => '\d+'], methods: ['PUT'])]
+    public function updateUser(Request                     $request,
+                               UserPasswordHasherInterface $passwordEncoder,
+                               ValidatorInterface          $validator,
+                               int                         $userId): JsonResponse
+    {
+        $user = $this->userRepository->find($userId);
+        if (!$user) {
+            return $this->respondNotFound("User not found");
+        }
+
+        $request = $request->toArray();
+
+        try {
+            if (isset($request['login'])) {
+                $login = $request['login'];
+                if (!$login) {
+                    throw new Exception();
+                }
+                if ($this->userRepository->findOneBy(['login' => $request['login']])) {
+                    return $this->respondValidationError('User with this login is already exist');
+                }
+
+                $user->setLogin($login);
+            }
+            if (isset($request['password'])) {
+                $password = $request['password'];
+                if (!$password) {
+                    throw new Exception();
+                }
+                $user->setPassword($password);
+            }
+            if (isset($request['fio'])) {
+                $user->setFio($request['fio']);
+            }
+            if (isset($request['roles'])) {
+                $user->setRoles([$request['roles']]);
+            }
+            if (isset($request['email'])) {
+                $user->setEmail($request['email']);
+            }
+
+            $validator->validate($user);
+
+            if (isset($request['password'])) {
+                $user->setPassword($passwordEncoder->hashPassword($user, $request['password']));
+            }
+
+            $this->em->flush();
+
+            return $this->respondWithSuccess("User updated successfully");
+        } catch (Exception) {
+            return $this->respondValidationError();
+        }
+    }
+
     #[Route('/{userId}', name: 'delete_by_id', requirements: ['userId' => '\d+'], methods: ['DELETE'])]
     public function deleteUser(int $userId): JsonResponse
     {
