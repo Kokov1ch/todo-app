a2e1721e6de67fefab785c6e7e6b8822c88ab794
kokov1ch <avk4125@mail.ru>
2023-10-09 02:15:52 +1000

    Add scss

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SignUp.css';
+import './SignUp.scss';
 
 const SignUp = () => {
     const navigate = useNavigate();
